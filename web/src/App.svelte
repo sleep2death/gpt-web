21790a45285abe0b66fbbcfd15605f6c1a9a047d
aspirin2d <aspirin2d@outlook.com>
2023-04-05 01:26:44 +0800

    init daisyui

---
+++ web/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Input from "./lib/input.svelte";
+  import LeftBtn from "./lib/left-btn.svelte";
+  import RightBtn from "./lib/right-btn.svelte";
+  import { input } from "./lib/store";
+</script>
+
+<main class="w-full relative">
+  <!-- Action Bar -->
+  <div class="w-full fixed bottom-0 flex flex-row justify-center items-center">
+    <div
+      class="navbar bg-base-300 md:rounded-box md:drop-shadow-md md:mb-6 max-w-3xl pb-6 md:pb-2 space-x-2"
+    >
+      <div class="justify-start"><LeftBtn /></div>
+      <div class="justify-center w-full"><Input input={$input} /></div>
+      <div class="justify-end"><RightBtn /></div>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ web/src/lib/input.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let input;
+</script>
+
+<div class="w-full p-2.5 rounded bg-base-100">
+  <div
+    contenteditable="true"
+    class="text-base-content w-full outline-base-200 outline-offset-8"
+    bind:innerHTML={input}
+  />
+</div>
